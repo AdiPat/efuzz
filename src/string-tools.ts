58b32bd444562a65f2222db0ada0308aca527246
Aditya Patange <contact.adityapatange@gmail.com>
2024-11-25 02:20:14 +0530

    feat: string-tools string to object similarity implementation

--- src/string-tools.ts
+++ src/string-tools.ts
@@ -16,3 +16,24 @@ export const computeSimilarity = (s: string, t: string): number => {
   const netScore = (levScore + jwScore) / 2;
   return netScore;
 };
+
+export const computeStringObjectSimilarity = (
+  query: string,
+  obj: any
+): number => {
+  let maxScore = 0;
+
+  const computeScore = (query: string, obj: any) => {
+    for (const key in obj) {
+      if (typeof obj[key] === "object" && obj[key] !== null) {
+        computeScore(query, obj[key]);
+      } else {
+        const curScore = computeSimilarity(query, String(obj[key]));
+        maxScore = Math.max(maxScore, curScore);
+      }
+    }
+  };
+
+  computeScore(query, obj);
+  return maxScore;
+};
