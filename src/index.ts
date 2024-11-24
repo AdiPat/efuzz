b74cb2e92968bbdbaa9074ee05f8287e85b91399
Aditya Patange <contact.adityapatange@gmail.com>
2024-11-25 00:53:09 +0530

    feat: initial api design and test setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export const main = () => {};
+export const efuzz = (_records: any[]) => {
+  const search = (_query: string, _options?: { threshold?: number }): any[] => {
+    return [];
+  };
+
+  return search;
+};
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,21 @@
 import { describe, it, expect } from "vitest";
-import { main } from "../index";
+import { efuzz } from "../index";
 
-describe("index", () => {
-  it("should have a main method", () => {
-    expect(main).toBeDefined();
+describe("efuzz should", () => {
+  it("have an efuzz method defined", () => {
+    expect(efuzz).toBeDefined();
+  });
+
+  it("called efuzz should return a search method", () => {
+    const search = efuzz([]);
+    expect(search).toBeDefined();
+  });
+
+  it("search method should return a list when given search query and threshold", () => {
+    const query = "test";
+    const threshold = 0.5;
+    const search = efuzz([]);
+    const result = search(query, { threshold });
+    expect(result).toEqual([]);
   });
 });
