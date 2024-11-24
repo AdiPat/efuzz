be2a08649dd0894fdbe2118af9eb978494ebdc14
Aditya Patange <contact.adityapatange@gmail.com>
2024-11-25 02:25:48 +0530

    feat: refactor efuzz
    - separate method to handle string and object searches
    - constant for default threshold

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,23 @@ import {
 } from "./string-tools";
 import { isArrayofObjects } from "./utils";
 
+export const efuzz = (records: any[]) => {
+  const search = async (
+    query: string,
+    options?: { threshold?: number }
+  ): Promise<any[]> => {
+    const threshold = validateAndGetThreshold(options?.threshold);
+
+    if (isArrayofObjects(records)) {
+      return handleObjectArraySearch(records, query, threshold);
+    }
+
+    return handleStringArraySearch(records, query, threshold);
+  };
+
+  return search;
+};
+
 const validateAndGetThreshold = (threshold: number | undefined): number => {
   if (!threshold) {
     console.log("Threshold not provided. Using default 0.5.");
@@ -20,30 +37,28 @@ const validateAndGetThreshold = (threshold: number | undefined): number => {
   return threshold;
 };
 
-export const efuzz = (records: any[]) => {
-  const search = async (
-    query: string,
-    options?: { threshold?: number }
-  ): Promise<any[]> => {
-    let { threshold } = options || {};
+const handleStringArraySearch = (
+  records: string[],
+  query: string,
+  threshold: number
+) => {
+  const results = records.filter((record) => {
+    const score = computeSimilarity(query, record);
+    return score > threshold;
+  });
 
-    threshold = validateAndGetThreshold(threshold);
+  return results;
+};
 
-    if (isArrayofObjects(records)) {
-      const results = records.filter((record) => {
-        const averageScore = computeStringObjectSimilarity(query, record);
-        return averageScore > threshold;
-      });
+const handleObjectArraySearch = (
+  records: any[],
+  query: string,
+  threshold: number
+) => {
+  const results = records.filter((record) => {
+    const averageScore = computeStringObjectSimilarity(query, record);
+    return averageScore > threshold;
+  });
 
-      return results;
-    }
-
-    const results = records.filter((record) => {
-      const score = computeSimilarity(query, record);
-      return score > threshold;
-    });
-    return results;
-  };
-
-  return search;
+  return results;
 };
