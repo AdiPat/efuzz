e5b042592589bfacf269af5aa1a8ef4162af2a50
Aditya Patange <contact.adityapatange@gmail.com>
2024-11-25 01:35:40 +0530

    feat: threshold validation tests

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,7 +1,16 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { efuzz } from "../index";
+import { afterEach, beforeEach } from "node:test";
 
 describe("efuzz should", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
   it("have an efuzz method defined", () => {
     expect(efuzz).toBeDefined();
   });
@@ -11,11 +20,35 @@ describe("efuzz should", () => {
     expect(search).toBeDefined();
   });
 
-  it("search method should return a list when given search query and threshold", () => {
+  it("search method should return a list when given search query and threshold", async () => {
     const query = "test";
     const threshold = 0.5;
     const search = efuzz([]);
-    const result = search(query, { threshold });
+    const result = await search(query, { threshold });
     expect(result).toEqual([]);
+  });
+
+  it.each([-1, -0.5, 1.01, 4, 100])(
+    "throw an error if invalid threshold (%s) is passed",
+    async (threshold) => {
+      const query = "test";
+      const search = efuzz([]);
+      const promise = search(query, { threshold });
+      await expect(promise).rejects.toThrowError(
+        "Invalid threshold value. Threshold must be a number between 0 and 1."
+      );
+      ``;
+    }
+  );
+
+  it("use threshold as 0.5 if no threshold is passed", async () => {
+    const consoleSpy = vi.spyOn(console, "log");
+    const query = "test";
+    const search = efuzz([]);
+    const result = await search(query);
+    expect(consoleSpy).toHaveBeenCalledOnce();
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Threshold not provided. Using default 0.5."
+    );
   });
 });
