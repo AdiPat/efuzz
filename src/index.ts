764b784515447e8ce35f73ac69876086d01c3874
Aditya Patange <contact.adityapatange@gmail.com>
2024-11-25 01:35:50 +0530

    feat: threshold validation implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 export const efuzz = (_records: any[]) => {
-  const search = (_query: string, _options?: { threshold?: number }): any[] => {
+  const search = async (
+    _query: string,
+    options?: { threshold?: number }
+  ): Promise<any[]> => {
+    let { threshold } = options || {};
+
+    if (!threshold) {
+      console.log("Threshold not provided. Using default 0.5.");
+      threshold = 0.5;
+    }
+
+    if (threshold && (threshold < 0 || threshold > 1)) {
+      throw new Error(
+        "Invalid threshold value. Threshold must be a number between 0 and 1."
+      );
+    }
+
     return [];
   };
 
